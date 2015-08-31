f24e3fc9a9ed6eadd55950f57ae9d4f3fda873ed
陆春 <lu7965@gmail.com>
2015-08-31 12:23:00 +0800

    订单

--- David/js/main.js
+++ David/js/main.js
@@ -10,10 +10,9 @@ require.config({
 require(['jquery','window'],function($,hlw){
 
     var pageWindow = new hlw.HlWindow();
-    //pageWindow.alert({
-    //    content:"每周最多记录10堂美容课哦！"
-    //});
-    console.log("hl-operate")
+    pageWindow.alert({
+        content:"每周最多记录10堂美容课哦！"
+    });
     var pkdata =  {
         "pklist":
             [
