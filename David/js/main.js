a7b476557177b2c2453f42cde8e6df18002ec7f2
陆春 <lu7965@gmail.com>
2015-09-01 10:15:24 +0800

    dd

--- David/js/main.js
+++ David/js/main.js
@@ -10,14 +10,14 @@ require.config({
 require(['jquery','window'],function($,hlw){
 
     //g个人课程信息对象
-    var pageWindow = new hlw.HlWindow();
-    pageWindow.confirm({
-        content:"<div>你的美容课次数已经打败全国 <span class='hlw_num blw_pk_num'>50%</span> 的姐妹 <br>"+
-        " 太棒了，你共完成了 <span class='hlw_num blw_lessons_num'> 50堂</span> 美容课！"+
-        " </div>",
-        hasMask:false,
-        y:$('.hl-button').position().top +20+"px"
-    });
+    //var pageWindow = new hlw.HlWindow();
+    //pageWindow.confirm({
+    //    content:"<div>你的美容课次数已经打败全国 <span class='hlw_num blw_pk_num'>50%</span> 的姐妹 <br>"+
+    //    " 太棒了，你共完成了 <span class='hlw_num blw_lessons_num'> 50堂</span> 美容课！"+
+    //    " </div>",
+    //    hasMask:false,
+    //    y:$('.hl-button').position().top +20+"px"
+    //});
 
     //记录一堂美容课相关事件
     $(".hl-button").on("click",function(){
@@ -33,7 +33,12 @@ require(['jquery','window'],function($,hlw){
                     //成功的话，将出现加1的效果
                     plusOne.apply(that);
                     //同时更新完成信息
-                    pageWindow.update(data.nums)
+                    $(".hl_user_info")
+                        .find(".blw_pk_num")
+                        .html(data.pknum)
+                        .end()
+                        .find(".blw_lessons_num")
+                        .html(data.lessonsnum);
                 }else{
                     new hlw.HlWindow().alert({
                         content:"每周最多记录10堂美容课哦！"
